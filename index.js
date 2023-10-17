5c91013ee2965c344b7bbca674368b7a67545722
brevre <brendamakena09@gmail.com>
2023-10-17 10:10:46 +0200

    fin

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function isPalindrome(word) {
+  // Convert the word to lowercase to make the comparison case-insensitive
+  word = word.toLowerCase();
+
+  // Remove any non-alphabet characters using a regular expression
+  word = word.replace(/[^a-z]/g, "");
+
+  // Reverse the word and compare it with the original word
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
+}
+
+// Example usage:
+console.log(isPalindrome("racecar")); // Expecting: true
+console.log(isPalindrome("robot"));   // Expecting: false
 
 /* 
   Add your pseudocode here
